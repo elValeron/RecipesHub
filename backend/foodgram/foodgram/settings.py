152f642a08eb32f820f6019530a6fb9962681e7b
elvaleron <vbalashov89@gmail.com>
2023-10-12 23:08:23 +0200

    Sprint 15 deploy fix badge, csrf

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -48,7 +48,7 @@ DEBUG_TOOLBAR_CONFIG = {
 
 ROOT_URLCONF = 'foodgram.urls'
 
-CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_HOST', '')
+CSRF_TRUSTED_ORIGINS = ['https://elfood.ddns.net']
 
 TEMPLATES = [
     {
